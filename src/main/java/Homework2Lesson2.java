0f1c5a7d982bada7a0710555f175f6bf99f2f7f7
Maxim <19sparow91@mail.ru>
2021-11-04 01:24:11 +0400

    Урок второй, задание 1.

---
+++ src/main/java/Homework2Lesson2.java
@@ -0,0 +1,22 @@
+public class Homework2Lesson2 {
+
+    public static void main(String[] args) {
+        System.out.println(lawOfJouleLenz(25, 10, 15));
+    }
+
+    /** Закон Джоуля-Ленца
+     * Количество теплоты, выделяемое проводником, равно
+     * произведению квадрата силы тока, сопротивления проводника,
+     * и времени прохождения тока по проводнику. Где,
+     * @param i - сила тока, Aмпер
+     * @param r - сопротивление, Ом
+     * @param dt - время прохождения тока по проводнику, секунды
+     * @return q - количество теплоты, Джоули.
+     */
+    public static int lawOfJouleLenz(double i, int r, int dt){
+
+        double q = Math.pow(i, 2) * r * dt;
+        return (int) q;
+
+    }
+}
