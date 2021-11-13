e20839d654d18014450f33289c26e611c109524b
Painkiller63 <19sparow91@mail.ru>
2021-11-13 12:24:38 +0400

    Оптимизирован код с учетом замечаний.

--- src/main/java/lesson4/HomeworkLesson4ver2.java
+++ src/main/java/lesson4/HomeworkLesson4ver2.java
@@ -8,28 +8,25 @@ public class HomeworkLesson4ver2 {
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите целое положительное число:");
-        boolean b = scanner.hasNextInt();
-        if (b) {
+        if (scanner.hasNext()) {
             int n = scanner.nextInt();
             if (n == 0) {
                 System.out.println("Квадратный корень нуля равен 0");
+            } else if (n < 0) {
+                System.out.println("Вы ввели отрицательное число! Введите положительное число.");
             } else {
-                if (n < 0) {
-                    System.out.println("Вы ввели отрицательное число! Введите положительное число.");
-                } else {
-                    int i = 1;
-                    while (true) {
-                        double m = (double) n / i;
-                        if (m == i) {
-                            System.out.println("Квадратный корень числа " + n + " равен: " + i);
-                            break;
+                int i = 1;
+                while (true) {
+                    double m = (double) n / i;
+                    if (m == i) {
+                        System.out.println("Квадратный корень числа " + n + " равен: " + i);
+                        break;
+                    } else {
+                        if (m > i) {
+                            i++;
                         } else {
-                            if (m > i) {
-                                i++;
-                            } else {
-                                System.out.println("Квадратный корень числа " + n + ": нецелое число.");
-                                break;
-                            }
+                            System.out.println("Квадратный корень числа " + n + ": нецелое число.");
+                            break;
                         }
                     }
                 }
