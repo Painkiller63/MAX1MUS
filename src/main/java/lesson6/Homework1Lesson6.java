54fde37b7e9d7e8d97a40f9350b910c0a0d8200d
Painkiller63 <19sparow91@mail.ru>
2021-11-19 23:28:07 +0400

    Решение задания №1 к занятию №6.

--- src/main/java/lesson6/Homework1Lesson6.java
+++ src/main/java/lesson6/Homework1Lesson6.java
@@ -8,32 +8,31 @@ public class Homework1Lesson6 {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите целое положительное число: ");
-        while (!scanner.hasNextInt()) {
-            System.out.println("Вы не ввели число. Пожалуйста введите целое положительное число:");
-            scanner.next();
-        }
-        int n = scanner.nextInt();
-        if (n < 0) {
-            System.out.println("Вы ввели отрицательное число. Перезапустите программу и введите положительное число.");
-        } else {
-            double[] array = new double[n];
+        int length;
+        do {
+            System.out.println("Введите длину массива (целое положительное число: ");
+            while (!scanner.hasNextInt()) {
+                System.out.println("Вы не ввели число. Пожалуйста введите целое положительное число:");
+                scanner.next();
+            }
+            length = scanner.nextInt();
+        } while (length <= 0);
+
+        double[] array = new double[length];
             for (int i = 0; i < array.length; i++) {
                 array[i] = Math.random();
             }
             System.out.println(Arrays.toString(array));
             double minimal = minimum(array);
-            System.out.println("Минимальное значение в массиве равно: " + minimal);
             double maximal = maximum(array);
+            double mean = arithmetical(array);
+            System.out.println("Минимальное значение в массиве равно: " + minimal);
             System.out.println("Максимальное значение в массиве равно: " + maximal);
+            System.out.println("Среднее арифметическое значение в массиве равно: " + mean);
             /* Еще один вариант нахождения минимального и максимального значения - через сортировку массива
             Arrays.sort(array);
             System.out.println("Минимальное значение в массиве равно: " + array[0]);
             System.out.println("Максимальное значение в массиве равно: " + array[n-1]); */
-            double mean = arithmetical(array);
-            System.out.println("Среднее арифметическое значение в массиве равно: " + mean);
-
-        }
     }
 
     public static double minimum(double[] array) {
