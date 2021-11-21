8e4f1c4b732ed08adc122a55ca94725668a98504
Painkiller63 <19sparow91@mail.ru>
2021-11-21 23:32:34 +0400

    Доработано решение задания №2 к занятию №6 с учетом поступивших замечаний.

--- src/main/java/lesson6/Homework2Lesson6.java
+++ src/main/java/lesson6/Homework2Lesson6.java
@@ -19,28 +19,39 @@ public class Homework2Lesson6 {
         } while (number < 0);
 
         int[] defArray = new int[10];
-        int a = 0;
-        int b = 1;
-        defArray[0] = a;
-        defArray[1] = b;
-        for (int i = 2; i < defArray.length; i++) {
-            defArray[i] = a + b;
-            a = b;
-            b = defArray[i];
+        for (int i = 0; i < defArray.length; i++) {
+            defArray[i] = fibonacci(i);
         }
 
         if (number > defArray.length - 1) {
             int[] newArray = Arrays.copyOf(defArray, number + 1);
-            int c = defArray[defArray.length - 2];
-            int d = defArray[defArray.length - 1];
             for (int i = defArray.length - 1; i < newArray.length; i++) {
-                newArray[i] = c + d;
-                c = d;
-                d = newArray[i];
+                newArray[i] = fibonacci(i);
             }
             System.out.println(number + "-й член последовательности Фибоначчи равен: " + newArray[number]);
         } else {
             System.out.println(number + "-й член последовательности Фибоначчи равен: " + defArray[number]);
+        }
+    }
+
+    public static int fibonacci(int number) {
+        int a = 0;
+        int b = 1;
+        int f = 0;
+
+        if (number == 0) {
+            return a;
+        } else {
+            if (number == 1) {
+                return b;
+            } else {
+                for (int i = 2; i <= number; i++) {
+                    f = a + b;
+                    a = b;
+                    b = f;
+                }
+                return f;
+            }
         }
     }
 }
