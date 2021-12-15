12addd643989581134bda2fddd5f440b180b7362
Maxim <19sparow91@mail.ru>
2021-12-15 18:21:52 +0400

    Решение задания к занятию 11

--- src/main/java/lesson11/AutoType.java
+++ src/main/java/lesson11/AutoType.java
@@ -5,14 +5,13 @@ public enum AutoType {
     CARS, TRUCKS;
 
     public static AutoType getAutoType(Automobile auto) {
-        for (AutoType autoType : values()) {
-            if ((auto instanceof Cars)) {
-                return CARS;
-            } else {
-                if ((auto instanceof Trucks)) {
-                    return TRUCKS;
-                }
+        if ((auto instanceof Cars)) {
+            return CARS;
+        } else {
+            if ((auto instanceof Trucks)) {
+                return TRUCKS;
             }
-        } throw new IllegalStateException("Unexpected value: " + auto);
+        }
+        throw new IllegalStateException("Unexpected value: " + auto);
     }
 }
