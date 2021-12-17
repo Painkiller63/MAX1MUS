d1f0c88acfe32fa42ceeace39792b089257996ec
Maxim <19sparow91@mail.ru>
2021-12-17 23:17:23 +0400

    Решение задания к занятию 12

--- src/main/java/lesson12/ArrayRunner.java
+++ src/main/java/lesson12/ArrayRunner.java
@@ -1,9 +1,5 @@
 package lesson12;
 
-import lesson12.MyArray;
-import lesson12.MyArrayDataException;
-import lesson12.MyArraySizeException;
-
 public class ArrayRunner {
 
     public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
